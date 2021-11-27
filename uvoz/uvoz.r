fe3e17ddbe5a4b39efab4ae8e36ebd9578f77ac3
Petja-Murnik <petja.murnik@gmail.com>
2021-11-27 16:21:51 +0100

    uvozil prvo tabelo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,4 +1,16 @@
 # 2. faza: Uvoz podatkov
+library(readr)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(rvest)
 
+sl <- locale("sl", decimal_mark=".", grouping_mark=",")
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+padavine <- read_csv("podatki/mesecne_padavine_2.csv", na="...",
+              locale=locale(encoding="Windows-1250"),col_names =TRUE)
+padavine1 = pivot_longer(padavine,
+                        cols = padavine[1],
+                        names_to = "po",
+                        values_to = "P"
+                        )
