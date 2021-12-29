efe0e80c765a90ac585c1598d59fa18136ebb025
Petja-Murnik <petja.murnik@gmail.com>
2021-12-29 15:21:23 +0100

    narisal 4 grafe

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -17,7 +17,21 @@ g1 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec, y = padavine ) + 
 print(g1)
 
 
-##
+##temperature za vse
+g2 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec, y = temperature ) + geom_col(position = "dodge") + 
+  facet_wrap(. ~ naselje, ncol = 3)
+print(g2)
+
+
+##iz tabele 3 bi rad narisu kako grejo čez čas avg 
+g3 =ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_t) + geom_line() + 
+  facet_wrap(. ~ naselje, ncol = 3)
+print(g3)
+
+##identično za čez čas avg p
+g4 = ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_p) + geom_line() + 
+  facet_wrap(. ~ naselje, ncol = 3)
+print(g4)
 
 
 
