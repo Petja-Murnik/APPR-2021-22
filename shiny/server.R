c35ab05278f08bd212f9218077f3f908151ca96b
Petja-Murnik <petja.murnik@gmail.com>
2022-01-13 17:03:34 +0100

    pripravil funkcijo za shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,2 +1,36 @@
 library(shiny)
 
+
+
+narisi_grafek = function(naselje1, vrsta, leto1){
+  if (vrsta == "padavine"){
+  grafek = ggplot(tabela1%>%filter(leto == leto1)%>% filter(naselje == naselje1))+
+    aes(x = mesec_c, y = padavine) +
+    geom_col(position = "dodge",fill = "dark blue") + 
+    theme_classic() +
+    labs(
+      x = "Mesec",
+      y = "Mesečne padavine[mm/m^2]",
+      title = naselje1
+    )+
+    scale_x_continuous("Mesec",breaks = 1:12)
+  print(grafek)
+  }
+  else if (vrsta == "temperatura"){
+  grafek = ggplot(tabela1%>%filter(leto == leto1)%>% filter(naselje == naselje1))+
+    aes(x = mesec_c, y = temperature,group = 1) + geom_line()+
+    scale_x_continuous("Mesec",breaks = 1:12)+
+    labs(
+      x = "Mesec",
+      y = "Povprečna dnevna temperatura[°C]",
+      title = naselje1
+    )
+  print(grafek)
+  }
+  else{
+    print("OJIO")
+  }
+}
+
+
+
