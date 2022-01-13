04d1aa92eff5d50407c4d5e255601d2db4da9e5d
Petja-Murnik <petja.murnik@gmail.com>
2022-01-13 13:19:18 +0100

    narisal grafa kot da je klimatogram

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -8,41 +8,52 @@ library(viridis)
 source("uvoz/uvoz.r", encoding="UTF-8")
 ##dež za vse
  
-g1 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec_c, y = padavine ) + geom_col(position = "dodge") + 
-  facet_wrap(. ~ naselje, ncol = 3) 
+g1 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec_c, y = padavine) +
+  geom_col(position = "dodge",fill = "dark blue") + 
+  facet_wrap(. ~ naselje, ncol = 3)   + theme_classic() +
+  labs(
+    x = "Mesec",
+    y = "Mesečne padavine[mm/m^2]",
+    title = "Merjenja 20-ih meteoroloških postaj v letu 2001"
+  )
 print(g1)
-
+#TODO meseci na k osi 
 
 ##temperature za vse
 g2 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec_c, y = temperature,group = 1 ) + geom_line(position = "dodge") + 
-  facet_wrap(. ~ naselje, ncol = 3)
+  facet_wrap(. ~ naselje, ncol = 3)+ theme_classic() +
+  labs(
+    x = "Mesec",
+    y = "Povprečna dnevna temperatura[°C]",
+    title = "Merjenja 20-ih meteoroloških postaj v letu 2001"
+  )
 print(g2)
 
 ##iz tabele 3 bi rad narisu kako grejo čez čas avg 
-g3 =ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_t) + geom_line() + 
-  facet_wrap(. ~ naselje, ncol = 3)
-#print(g3)
+g3 =ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_p) + geom_line() + 
+  facet_wrap(. ~ naselje, ncol = 3) + theme_classic() 
+print(g3)
 
 ##identično za čez čas avg p
-g4 = ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_p) + geom_line() + 
-  facet_wrap(. ~ naselje, ncol = 3)
-#print(g4)
+g4 = ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_t) + geom_line(color="red") + 
+  facet_wrap(. ~ naselje, ncol = 3) + theme_classic() 
+print(g4)
 
 
 #temperatur za vse po vseh mesecih iz tabele 1 
 g5 = ggplot(tabela1) + aes(x = datum_c , y = temperature, group = 1) +
-  geom_line() + facet_wrap(. ~ naselje, ncol = 3) 
-#print(g5)
+  geom_line() +facet_wrap(. ~ naselje, ncol = 3) 
+print(g5)
 
 #dež za vse po vseh mesecih iz tbele 1
 g6 = ggplot(tabela1) + aes(x = datum_c , y = padavine) +
-  geom_col() + facet_wrap(. ~ naselje, ncol = 3) 
+  geom_line() + facet_wrap(. ~ naselje, ncol = 3) 
 print(g6)
 
 ##
 g7 = ggplot(tabela3%>%filter(leto=="2010")) + aes(x=nmv , y = avg_t)+ 
   geom_point() + stat_smooth(method = lm) 
-#print(g7)
+print(g7)
 
 
 ##
@@ -77,7 +88,7 @@ g12 = ggplot() +
   geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
   geom_point( data=data1, aes(x=lon, y=lat, size=avg_preb,color=nmv)) +
   scale_size_continuous(range=c(1,12)) +
-  scale_color_viridis(trans="log") +
+  scale_colour_viridis(trans="log",option = "C") +
   theme_void() + ylim(45,47) + coord_map()  +
   geom_text(
     data = data1,
@@ -92,7 +103,7 @@ g13 = ggplot() +
   geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
   geom_point( data=data2, aes(x=lon, y=lat, size=avg_p,color=nmv)) +
   scale_size_continuous(range=c(1,12)) +
-  scale_color_viridis(trans="log") +
+  scale_color_viridis(trans="log",option = "C") +
   theme_void() + ylim(45,47) + coord_map()  +
   geom_text(
     data = data2,
@@ -107,17 +118,35 @@ g14 = ggplot() +
   geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
   geom_point( data=data3, aes(x=lon, y=lat, size=avg_t,color=nmv)) +
   scale_size_continuous(range=c(1,12)) +
-  scale_color_viridis(trans="lin") +
+  scale_color_viridis(trans="log",option = "C") +
   theme_void() + ylim(45,47) + coord_map()  +
   geom_text(
     data = data3,
-    mapping = aes(x = lon , y = lat+0.07, label = naselje),
+    mapping = aes(x = lon , y = lat+0.09, label = naselje),
     size = 2.5
   )
-#print(g14)
+print(g14)
 
 
 ##
 g15 = ggplot(tabela2%>%group_by(naselje)) + aes(x=datum, y=gostota,group = 1)  +
   geom_line() + facet_wrap(.~ naselje, ncol = 3)
-#print(g15)
+print(g15)
+
+
+
+gg =  ggplot()+ layer(
+    data = tabela3%>%group_by(naselje),
+    mapping = aes(x = leto, y = avg_t),
+    geom = "line",
+    stat = "identity",
+    position = "identity"
+  ) + layer(
+    data = tabela3%>%group_by(naselje),
+    mapping = aes(x = leto, y = avg_p),
+    geom = "line",
+    stat = "identity",
+    position = "identity",
+  ) +   facet_wrap(.~ naselje ,ncol = 3)
+
+
