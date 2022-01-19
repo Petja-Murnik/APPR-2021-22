8e72c308b2e5d6f4901b74e472dfc0ca44026541
Petja-Murnik <petja.murnik@gmail.com>
2022-01-20 00:13:46 +0100

    Uredil kodo

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -4,9 +4,10 @@ library(ggplot2)
 library(dbplyr)
 library(maps)
 library(viridis)
-#source("uvoz/uvoz.r", encoding="UTF-8")
-##dež za vse
- 
+
+#OPOMBA: Kjer je #======== je graf tudi v poročilu
+
+#dež za vse
 g1 = ggplot(tabela1%>%filter(leto == "2010")) + aes(x = mesec_c, y = padavine) +
   geom_col(position = "dodge",fill = "dark blue") + 
   facet_wrap(. ~ naselje, ncol = 3)   + theme_classic() +
@@ -16,8 +17,6 @@ g1 = ggplot(tabela1%>%filter(leto == "2010")) + aes(x = mesec_c, y = padavine) +
     title = "Merjenja 20-ih meteoroloških postaj v letu 2001"
   )+
   scale_x_continuous("Mesec",breaks = 1:12)
-#print(g1)
-#TODO meseci na k osi 
 
 ##temperature za vse
 g2 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec_c, y = temperature,group = 1 ) + geom_line(position = "dodge") + 
@@ -27,78 +26,48 @@ g2 = ggplot(tabela1%>%filter(leto == "2001")) + aes(x = mesec_c, y = temperature
     y = "Povprečna dnevna temperatura[°C]",
     title = "Merjenja 20-ih meteoroloških postaj v letu 2001"
   )
-#print(g2)
 
-##iz tabele 3 bi rad narisu kako grejo čez čas avg 
+#Kako grejo čez leta padavine LETNO , Ali naj uporabim ????
 g3 =ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_p) + geom_line() + 
   facet_wrap(. ~ naselje, ncol = 3) + theme_classic() 
-#print(g3)
 
-##identično za čez čas avg p
+#identično za čez čas temperature LETNO
 g4 = ggplot(tabela3%>%group_by(naselje)) + aes(x = leto, y = avg_t) + geom_line(color="red") + 
   facet_wrap(. ~ naselje, ncol = 3) + theme_classic() 
-#print(g4)
 
-
-##
+#razsevni, kako so temperature letne glede na nmv
+#OP.: Identičen je pri analizi narisan
 g7 = ggplot(tabela3) + aes(x=nmv , y = avg_t)+ 
   geom_point() + stat_smooth(method = lm) 
-#print(g7)
 
 
-##
-
+#razsevni, kako so padavine letne glede na nmv
+#OP.: Identičen je pri analizi narisan
 g8 = ggplot(tabela3) + aes(x=nmv , y = avg_p)+ 
   geom_point() + stat_smooth(method = lm) 
-#print(g8)
 
-##
+#====gostota poseljenosti od nmv ====
 g9 = ggplot(tabela3) + aes(x=nmv , y = avg_preb,color=naselje)+ 
   geom_point(size = 2.5) + 
   labs(
     x = "Nadmorska višina[m]",
     y = "Gostota poseljenosti [prebivalci/km^2]",
-    title = "Gostota poseljenosti",
-    color = "Naselje"
+    title = "Gostota poseljenosti"
   ) 
-#print(g9)
+#==========================================
 
-##
+#Kako je gostota poseljenosti od temperatur, je tudi pri analizi
 g10 = ggplot(tabela3[tabela3$naselje != "Kredarica",]) + aes(x=avg_t , y = avg_preb)+ 
   geom_point() + stat_smooth(method = lm)
-#print(g10)
 
-## 
+
+#Kako je gostota poseljenosti od padavin, je tudi pri analizi
 g11 = ggplot(tabela3[tabela3$naselje != "Kredarica",]) + aes(x=avg_p , y = avg_preb)+ 
   geom_point() + stat_smooth(method = lm)
-#print(g11)
 
-##
-#TODO risati na zemljavide
-
+#===================================
 SLO <- map_data("world") %>% filter(region=="Slovenia")
 data = tabela3 %>%filter(leto=="2010")
-#data_12 = rbind(data , JSC)
-#data_12 =data_12[data_12$naselje != "Slap pri Vipavi",]
-#data_12 = data_12[data_12$naselje != "Brnik",]
-#g12 = ggplot() +
-#  geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
-#  geom_point( data=(data_12), aes(x=lon, y=lat, size=avg_preb,color=nmv)) +
-#  scale_size_continuous(range=c(1,12)) +
-#  scale_colour_viridis(trans="log",option = "C") +
-#  theme_void() + ylim(45,47) + coord_map()  +
-#  geom_text(
-#    data = data_12,
-#    mapping = aes(x = lon , y = lat+0.07, label = naselje),
-#    size = 2.5
-#  ) + 
-#  labs(
-#    size = "Gostota poseljenosti",
-#    color = "Nadmorska višina"
-#  )
-#print(g12)
-
-##
 data2 = data[-c(17,18),]
 g13 = ggplot() +
   geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
@@ -116,9 +85,7 @@ g13 = ggplot() +
     colour = "Nadmorska višina[m]",
     size = "Povprečne mesečne \npadavine[mm/m^2]"
   )
-#print(g13)    
-
-##
+#===============================================
 data3 = data[-17,]
 g14 = ggplot() +
   geom_polygon(data = SLO, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
@@ -136,12 +103,11 @@ g14 = ggplot() +
     colour = "Nadmorska višina[m]",
     size = "Povprečne dnvene \ntemperature[°C]"
   )
+#===============================================
 
-
-##
+#Kako gre gostota prebivalstva čez leta, brezveze
 g15 = ggplot(tabela2%>%group_by(naselje)) + aes(x=datum, y=gostota,group = 1)  +
   geom_line() + facet_wrap(.~ naselje, ncol = 3)
-
 
 #spremembe tamperatur tekom let
 gg =  ggplot() + layer(
@@ -165,7 +131,7 @@ ylim.prim <- c(0, 100)
 ylim.sec <- c(0, 8)
 b <- diff(ylim.prim)/diff(ylim.sec)
 a <- ylim.prim[1] - b*ylim.sec[1]
-#=================
+#======================
 G1 = ggplot(tabela1%>%filter(leto == "2010")%>%
               filter(naselje!= "Rogaška Slatina")%>%filter(naselje != "Slap pri Vipavi" ),
             aes(mesec_c, padavine)) + 
